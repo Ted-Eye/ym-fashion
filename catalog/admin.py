6138aa38652037c285981c55573b16cb7c549203
Ted-Eye <booktedeye@gmail.com>
2026-03-23 22:21:08 +0100

    minor adjustments

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,7 @@ from .models import Hairstyle, Product
 
 # Register your models here.
 
-#username:....admin password:...admin2026$$
+#username:....admin password:..admin2026$$
 
 admin.site.register(Hairstyle)
 admin.site.register(Product)
