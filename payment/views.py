5ade09b3bb2ceffaae0443f4c4268ab9b60a5bfa
Ted-Eye <booktedeye@gmail.com>
2026-03-25 08:58:14 +0100

    Adjusted payment view to send error to front end for quick debugging

--- payment/views.py
+++ payment/views.py
@@ -3,36 +3,43 @@ from rest_framework.response import Response
 from .services import CampayService
 from .models import Payment
 from rest_framework.permissions import AllowAny
-import logging
 
-logger = logging.getLogger(__name__)
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def initiate_payment(request):
-    phone = request.data.get("phone")
-    amount = request.data.get("amount")
-    service = CampayService()
-    response = service.initiatePayment(
-        amount=amount, phone=phone, description='Barbing appointment'
-    )
-    # FOR DEBUG PURPOSES
-    logger.error(f"Campay response: {response}")
-    # logger.error(f"Error: {str(e)}")
-    reference = response.get("reference") if isinstance(response, dict) else None
-
-    if not response:
+    try:
+        phone = request.data.get("phone")
+        amount = request.data.get("amount")
+        service = CampayService()
+        response = service.initiatePayment(
+            amount=amount, phone=phone, description='Barbing appointment'
+        )
+    # TEMP DEBUG
         return Response({
-            "error": "payment initiation failed",
-            "details": str(response)
-        }, status=400)
-
-    payment = Payment.objects.create(
-        reference=reference, phone=phone, amount=amount, status="PENDING"
-    )
-    return Response({
-        "message": "Payment initiated", "reference": reference
+        "debug_response": str(response)
     })
+    except Exception as e:
+        return Response({
+            "error": str(e)
+        }, status=500)
+
+
+    
+    # reference = response.get("reference") if isinstance(response, dict) else None
+
+    # if not response:
+    #     return Response({
+    #         "error": "payment initiation failed",
+    #         "details": str(response)
+    #     }, status=400)
+
+    # payment = Payment.objects.create(
+    #     reference=reference, phone=phone, amount=amount, status="PENDING"
+    # )
+    # return Response({
+    #     "message": "Payment initiated", "reference": reference
+    # })
 
 @api_view(["GET"])
 def check_payment_status(request, reference):
